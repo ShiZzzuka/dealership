02d9eb7cfba2cfbeb9ab792f363ef692066f2dc8
ShiZzzuka <radmehrshabanii@gmail.com>
2022-03-07 20:05:29 +0330

    before brand scaffold

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,6 +1,20 @@
 class BrandsController < ApplicationController
+	before_action :set_brand, only: %i[ show ]
 
-	def edit
-		@brands = Brand.all 
+	def index
+	 @brands = Brand.all
 	end
+
+	def show; end
+
+	def new
+		@brand = brand.new
+	end
+	
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_brand
+		@brand = Brand.find(params[:id])
+	end
+
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ApplicationRecord
 	has_many :cars
+	has_one_attached :image
 end
